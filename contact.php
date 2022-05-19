a77ce4b151736aabffc6ba4bfd7394f3593ed350
Michal Šesták <lertcz@gmail.com>
2022-05-19 11:23:22 +0200

    UI update 2

--- contact.php
+++ contact.php
@@ -1,4 +1,15 @@
-<?php
-echo "kontakt na autora";
-echo '<a href="./index.html">Back</a>';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <?php
+    echo "kontakt na autora";
+    echo '<a href="./index.html">Back</a>';
+    ?>
+</body>
+</html>
